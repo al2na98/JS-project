8cc64dbdcdd4dde0640ec62a9c22dad5230a77bd
Alena <dudoladovaal75@gmail.com>
2025-10-14 17:34:43 +0700

    debugging-1-done

--- weatherApp1/app.js
+++ weatherApp1/app.js
@@ -1,1 +1,9 @@
-i
+const cityInput = document.getElementById("citySearch");
+
+  // событие при нажатии Enter (поиск)
+  cityInput.addEventListener('keypress', (event) => {
+    if (event.key === 'Enter') {
+      event.preventDefault(); // чтобы форма не отправлялась
+      console.log('Город:', cityInput.value);
+    }
+  });
